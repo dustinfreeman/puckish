8a535a2165317b06d9c03a6e873590885d116d4e
Dustin Freeman <dustin.freeman@gmail.com>
2023-06-12 06:40:23 +0300

    physics

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallControl : MonoBehaviour
+{
+
+  public void OnJump(InputValue value)
+  {
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+    if (value.isPressed)
+    {
+      var rb = this.GetComponent<Rigidbody>();
+      rb.AddForce(Vector3.left * 50);
+    }
+#endif
+  }
+}
