e444ecf008073dcc6b4ef985497af8fa9daa61a1
Dustin Freeman <dustin.freeman@gmail.com>
2023-06-15 12:08:12 +0300

    Label HUDs look much better now

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -5,14 +5,21 @@ public class HUD : MonoBehaviour {
   GameObject HudPlane;
 
   void FixedUpdate() {
-    RaycastHit[] hits = Physics.SphereCastAll(
-      transform.position, 0.4f, transform.forward,
-      Mathf.Infinity, Physics.AllLayers, QueryTriggerInteraction.Collide);
     string hudString = "";
     Vector3 closestHit = transform.position + (transform.forward * 10000);
+    RaycastHit[] hits = Physics.SphereCastAll(
+      transform.position,
+      0.4f, transform.forward,
+      Mathf.Infinity, Physics.AllLayers, QueryTriggerInteraction.Collide);
     foreach (var _hit in hits) {
       if (_hit.collider.GetComponent<TargetCollider>() ||
         _hit.collider.GetComponent<Ball>()) {
+
+        if (Puck.Instance.CurrentBall == _hit.collider.GetComponent<Ball>()) {
+          //exclude the current ball
+          continue;
+        }
+
         if ((transform.position - closestHit).magnitude >
             (transform.position - _hit.point).magnitude
           ) {
@@ -21,12 +28,24 @@ public class HUD : MonoBehaviour {
         }
       }
     }
+
+    //RaycastHit hit;
+    //Physics.SphereCast(transform.position, 0.4f, transform.forward,
+    //  out hit,
+    //  Mathf.Infinity, Physics.AllLayers, QueryTriggerInteraction.Collide);
+    //if (hit.collider.GetComponent<Ball>() || hit.collider.GetComponent<TargetCollider>()) {
+    //  hudString = hit.collider.name;
+    //  closestHit = hit.point;
+    //}
+
     HudPlane.SetActive(hudString.Length > 0);
     if (hudString.Length > 0) {
-      //Debug.LogFormat("closestHit {0}", hudString);
-      HudPlane.transform.position = closestHit;
-      HudPlane.transform.rotation = transform.rotation;
-      HudPlane.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = hudString;
+      Debug.LogFormat("closestHit {0}", hudString);
+      HudPlane.transform.SetPositionAndRotation(closestHit, transform.rotation);
+      var text = HudPlane.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+      text.text = hudString;
+      float textScale = Mathf.Sqrt((transform.position - closestHit).magnitude);
+      text.transform.localScale = Vector3.one * textScale;
     }
   }
 }
